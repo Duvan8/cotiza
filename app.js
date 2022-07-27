7b6701847d04b151d75882d5fd64113993f08699
Duvan8 <depenuela8@misena.edu.co>
2022-07-27 08:41:43 -0500

    hola

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const path = require("path");
 const app = express();
 const session = require('express-session'); 
+const PORT = process.env.PORT || 3000;
 
 app.use(session({
     secret: '123',
@@ -15,13 +16,14 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 
 
-app.set('port',3000);
+
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'vistas/'));
 
 app.use(require('./rutas/rutas'));
 
-app.listen(app.get('port'), () => {
-    console.log("Se esta ultilizando el puerto",app.get('port'));
+
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
 
